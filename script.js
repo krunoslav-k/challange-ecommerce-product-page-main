e67e8f45135238e7538c910b1ca5799af01e2dd6
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2025-06-24 11:35:22 +0200

    feat: add thumbnails rendering to lightbox

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ document.body.append(lightboxEl);
 
 const mainImage = document.querySelector(".main-image");
 const lightboxMainImage = document.createElement("img");
+const thumbnailEls = document.querySelectorAll(".thumbnail");
+const lightboxThumbnails = [];
 
 mainImage.addEventListener("click", () => {
   lightboxEl.classList.add("active");
   lightboxMainImage.classList.add("lightbox-main-image");
   lightboxMainImage.src = mainImage.src;
   lightboxEl.append(lightboxMainImage);
+  renderThumbnails();
 });
 
 lightboxEl.addEventListener("click", (e) => {
@@ -20,3 +23,26 @@ lightboxEl.addEventListener("click", (e) => {
     lightboxMainImage.remove();
   }
 });
+
+thumbnailEls.forEach((thumbnailEl) => {
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("thumbnail-wrapper");
+  const img = document.createElement("img");
+  img.classList.add("thumbnail");
+  img.src = thumbnailEl.src;
+  wrapper.append(img);
+  lightboxThumbnails.push(wrapper);
+});
+
+// za thmbnails (mozda u neku funkciju ili vjv razdijeliti u vise funkcija)
+
+const lightboxThumbnailsWrapper = document.createElement("div");
+lightboxThumbnailsWrapper.classList.add("thumbnails-wrapper");
+
+function renderThumbnails() {
+  lightboxThumbnails.forEach((thumbnail) => {
+    lightboxThumbnailsWrapper.append(thumbnail);
+  });
+  lightboxMainImage.after(lightboxThumbnailsWrapper);
+  console.log(lightboxMainImage);
+}
